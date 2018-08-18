3d395138bdebe15531eb052a7b061b23f2c06e0f
korsikova_ana <nastcka@gmail.com>
2018-08-18 14:58:50 +0300

    the whole site is done

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,11 +1,18 @@
-function Menu(menuBtnClass, menuListClass, menuItemsClass,  menuBtnPressClass, menuListVisibilityClass) {
+function Menu(menuBtnClass, menuListClass,  menuBtnPressClass, menuListVisibilityClass) {
     var menuButton = document.getElementsByClassName(menuBtnClass)[0],
-        menuList = document.getElementsByClassName(menuListClass)[0],
-        menuItems = document.getElementsByClassName(menuItemsClass);
+        menuList = document.getElementsByClassName(menuListClass)[0];
 
     menuButton.addEventListener('click', function(event) {
         menuButton.classList.toggle(menuBtnPressClass);
         menuList.classList.toggle(menuListVisibilityClass);
-    })
+
+        event.stopPropagation();
+
+        document.addEventListener('click', function menuActivated(event) {
+            menuButton.classList.remove(menuBtnPressClass);
+            menuList.classList.remove(menuListVisibilityClass);
+            document.removeEventListener('click', menuActivated);
+        });
+    });
 }
 
--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,14 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var menu = new Menu('menu__button-hidden', 'menu', 'menu__item', 'menu__button_press_pressed', 'menu__list_visibility_visible');
+    var menu = new Menu('menu__button-hidden', 'menu',  'menu__button_press_pressed', 'menu__list_visibility_visible');
 });
-function Menu(menuBtnClass, menuListClass, menuItemsClass,  menuBtnPressClass, menuListVisibilityClass) {
+function Menu(menuBtnClass, menuListClass,  menuBtnPressClass, menuListVisibilityClass) {
     var menuButton = document.getElementsByClassName(menuBtnClass)[0],
-        menuList = document.getElementsByClassName(menuListClass)[0],
-        menuItems = document.getElementsByClassName(menuItemsClass);
+        menuList = document.getElementsByClassName(menuListClass)[0];
 
     menuButton.addEventListener('click', function(event) {
         menuButton.classList.toggle(menuBtnPressClass);
         menuList.classList.toggle(menuListVisibilityClass);
-    })
+
+        event.stopPropagation();
+
+        document.addEventListener('click', function menuActivated(event) {
+            menuButton.classList.remove(menuBtnPressClass);
+            menuList.classList.remove(menuListVisibilityClass);
+            document.removeEventListener('click', menuActivated);
+        });
+    });
 }
 
