f5c2a30e7a20c5bcd29ace6e2392c52b365ccb00
Igor Korsikov <korsikov009@gmail.com>
2018-09-10 15:30:22 +0300

    changed form a bit

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var menu = new Menu('menu__button-hidden', 'menu', 'menu__button_press_pressed', 'menu__list_visibility_visible');
+    var form = new AjaxForm('form');
 })
 
 
